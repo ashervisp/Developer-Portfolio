bfd1c7ef538fafd0600a4b2f683be638153650d1
Asher <ashvisperas@gmail.com>
2021-01-30 02:46:57 -0600

    fixed years and saving

--- schedule_project/scheduler.js
+++ schedule_project/scheduler.js
@@ -1,33 +1,44 @@
 
 const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
+let firstMonth = findMonth();
+let monthCounter = firstMonth;
+let greenDays = []
+let checkMonth = 0
+let day = new Date();
+let currYear = day.getFullYear()
+let storeTitle= ''
+let dateData = {};
 //Changes the title to the current month
 function findTitle(month){
-  let day = new Date();
   let currentTitle = document.querySelector('#title');
-
-  console.log(currentTitle);
-  currentTitle.innerHTML = months[month] + ' ' +  day.getFullYear() + ': ';
+  console.log(checkMonth)
+  console.log(month)
+  if(checkMonth == 11 && month == 0){
+    currYear = currYear + 1;
+    checkMonth = 0;
+  } else if (checkMonth == 0 && month == 11){
+    currYear = currYear - 1;
+    checkMonth = 11;
+  }
+  else{
+  checkMonth = month;
+  }
+  storeTitle = months[month] + currYear;
+  currentTitle.innerHTML = months[month] + ' ' +  currYear + ': ';
 }
-
 //Places the day number in the calendar
 function createDay(x) {
   let dateNumber = document.querySelectorAll('.day')
   let date = new Date();
   let firstDay = new Date(date.getFullYear(),x,1);
-  console.log(x);
-  console.log(date.getMonth())
-  console.log(daysInMonth(x+1,2021))
   for(let i = 0; i < daysInMonth(x+1,2021);i++){
     dateNumber[firstDay.getDay()+i].innerHTML = i+1;
   }
 }
-
 //Finds the days in that specific month
 function daysInMonth(month,year){
   return new Date(year,month,0).getDate();
 }
-
 //Changes day color when task is complete i.e clicked on
 function taskDone(el){
   if(el.innerHTML == ''){
@@ -38,48 +49,62 @@ function taskDone(el){
   }else{
   el.style.backgroundColor = 'green';
   }
-  console.log('change');
-};
+}
 
 function findMonth(){
   let date = new Date()
-  console.log(date.getMonth())
   return date.getMonth();
 }
 
-let greenDays = []
-
-function storeDoneTasks(){
+function storeDoneTasks(month){
   let dateNumber = document.querySelectorAll('.day');
+  let currMonthYear = months[month] + currYear.toString();
+
 
   for(let i = 0;i  < 42; i++){
     if(dateNumber[i].style.backgroundColor == 'green'){
       greenDays.push(1);
-    }else greenDays.push(0);
+    }else
+      greenDays.push(0);
   }
 
-  console.log(greenDays)
+  dateData[currMonthYear] = greenDays;
+  greenDays= [];
 }
 
-let firstMonth = findMonth();
-let monthCounter = firstMonth;
+function showDoneTasks(){
+  let savedDays = document.querySelectorAll('.day')
+  for(let k in dateData){
+    console.log(k)
+    console.log(storeTitle)
+    console.log(dateData)
+    if( k == storeTitle){
+      for(let m = 0; m < 42; m++){
+        if(dateData[k][m] == 1){
+          savedDays[m].style.backgroundColor = 'green';
+        }
+      }
+      break;
+    }
+  }
+}
 
 //Changes the month dates
 function nextMonth(){
-  storeDoneTasks();
+  storeDoneTasks(monthCounter);
   clearCalendar();
   monthCounter++;
-  console.log(monthCounter);
   createDay(monthCounter)
   if(monthCounter > 11){
     monthCounter = 0;
   }
   findTitle(monthCounter)
-
+  showDoneTasks();
   return;
 }
 
 function prevMonth(){
+  storeDoneTasks(monthCounter);
   clearCalendar();
   monthCounter--;
   createDay(monthCounter);
@@ -87,6 +112,7 @@ function prevMonth(){
     monthCounter = 11
   }
   findTitle(monthCounter);
+  showDoneTasks();
   return;
 }
 
@@ -100,8 +126,6 @@ function clearCalendar(){
     nextTitle.innerHTML = '';
     return;
 }
-
-
 
 createDay(findMonth());
 findTitle(findMonth());
